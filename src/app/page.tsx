c46b56e94fdbf8fe5dcc7d836fd6bf70c9908fb0
Toni Pham <139079071+tonidevvn@users.noreply.github.com>
2025-03-28 02:32:58 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { BackgroundEffects } from "@/components/BackgroundEffects";
 import { FAQ } from "@/components/FAQ";
 import { Features } from "@/components/Features";
 import { Footer } from "@/components/Footer";
-import { Gallery } from "@/components/Gallery";
 import { Hero } from "@/components/Hero";
 import { HowItWorks } from "@/components/HowItWorks";
 import { Navigation } from "@/components/Navigation";
